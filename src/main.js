edc53b1616f63a8e6fce4e58bb1db507257331d9
Erika Silva <essilva@thoughtworks.com>
2018-03-12 10:27:21 -0300

    Test husky plugin

--- src/main.js
+++ src/main.js
@@ -7,3 +7,20 @@ function sum() {
 }
 
 sum();
+
+const cidade = 'Manaus';
+console.log('Eu moro em ', cidade);
+console.log(`Eu moro em ${cidade}`);
+
+const fruits = 'banana'
+        + '\n'
+        + 'orange'
+        + '\n'
+        + 'apple';
+
+const newFruits =
+`banana
+orange
+apple`;
+
+console.log(newFruits);
